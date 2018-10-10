d90bdab6e9a3e6e709ed121e9ca6454d7e047e28
Unknown <miguel.pimenta@Outlook.com>
2018-10-10 13:09:01 +0100

    Working

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,6 @@ __package__ = 'app'
 from flask import Flask
 
 app = Flask(__name__)
+
+from router import router
+
---
+++ app/router/router.py
@@ -0,0 +1,90 @@
+from app import app
+from flask import request, jsonify, render_template
+import json
+import ast
+import imp
+
+# Root
+@app.route("/")
+def root_response():
+    message = {        
+        'status': '200',
+        'message': 'Welcome to the Python Rest API',
+        'apiVersion': 'Version 1.0',
+        'by': 'Miguel Pimenta'
+    }
+    #return jsonify(message)
+    return render_template('index.html', message=message)
+
+# Create User
+@app.route("/users", methods=['POST'])
+def create_user(): 
+    message = {        
+        'status': '501',
+        'message': 'Create User Not implemented.',        
+    }
+    return jsonify(message), 501    
+
+# Read User
+@app.route("/users/<user_id>", methods=['GET'])
+def read_user(user_id):
+    message = {        
+        'status': '501',
+        'message': 'Read User Not implemented.',        
+    }
+    return jsonify(message), 501 
+
+# Update User
+@app.route("/users/<user_id>", methods=['POST'])
+def update_user(user_id):
+    message = {        
+        'status': '501',
+        'message': 'Update User Not implemented.',        
+    }
+    return jsonify(message), 501
+
+# Delete Record
+@app.route("/users/<user_id>", methods=['DELETE'])
+def remove_user(user_id):
+    message = {        
+        'status': '501',
+        'message': 'Delete Record Not implemented.',        
+    }
+    return jsonify(message), 501
+
+# List Users
+@app.route("/users", methods=['GET'])
+def list_search_users():         
+    message = {        
+        'status': '501',
+        'message': 'List/Search Users Not implemented.',        
+    }
+    return jsonify(message), 501
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    print(str(e))
+    message = {        
+        'status': '400',
+        'message': 'Bad request.',        
+    }
+    return jsonify(message), 400
+
+@app.errorhandler(404)
+def page_not_found(e):
+    print(str(e))
+    message = {        
+        'status': '404',
+        'message': 'This route is currently not supported.'        
+    }
+    return jsonify(message), 404
+
+@app.errorhandler(501)
+def not_implemented(e):
+    print(str(e))
+    message = {        
+        'status': '501',
+        'message': 'Internal server error / Not implemented.',        
+    }
+    return jsonify(message), 501
